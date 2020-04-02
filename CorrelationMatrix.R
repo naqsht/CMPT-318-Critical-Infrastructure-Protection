c380836914949b14dccb0d1ef0c80d4d323d2e4b
xiyuz <xiyu_zhang@sfu.ca>
2020-04-01 17:15:43 -0700

    CorrelationMatrix

--- CorrelationMatrix.R
+++ CorrelationMatrix.R
@@ -1,26 +1,28 @@
 library(corrplot)
 setwd('E:\\OneDrive - sfu.ca\\CMPT 318 Cyber Security\\cmpt318-project')
-data <- read.csv("TrainData.txt", header = TRUE, sep = ",", dec = ".")
-# eliminate all NA's
-i<-1
-while(i <= nrow(data))
-{
-  #print(i)
-  if(is.na(data[i, 3])||is.na(data[i, 4])||is.na(data[i, 5])||is.na(data[i, 6])||is.na(data[i, 7])||is.na(data[i, 8])||is.na(data[i, 9]))
-  {
-    data<- data[-c(i), ]
-    
-  }
-  else
-  {
-    i = i + 1
-  }
-  
-}
-print(data)
 
+# data <- read.csv("TrainData.txt", header = TRUE, sep = ",", dec = ".")
+# # eliminate all NA's
+# i<-1
+# while(i <= nrow(data))
+# {
+#   #print(i)
+#   if(is.na(data[i, 3])||is.na(data[i, 4])||is.na(data[i, 5])||is.na(data[i, 6])||is.na(data[i, 7])||is.na(data[i, 8])||is.na(data[i, 9]))
+#   {
+#     data<- data[-c(i), ]
+#   }
+#   else
+#   {
+#     i = i + 1
+#   }
+#   
+# }
+#write.table(data, "data_without_NA.txt", append = FALSE, sep = ",", dec = ".",
+#            row.names = FALSE, col.names = TRUE)
+
+
+data <- read.csv("data_without_NA.txt", header = TRUE, sep = ",", dec = ".")
 count <- nrow(data)
-
 Global_active_power<-data.frame(c(data[1:count, 3]))
 Global_reactive_power<-data.frame(c(data[1:count, 4] ))
 Voltage<-data.frame(c(data[1:count, 5]))
@@ -48,7 +50,7 @@ rownames(correlation_mat)<-c("Global_active_power","Global_reactive_power","Volt
 colnames(correlation_mat)<-c("Global_active_power","Global_reactive_power","Voltage","Global_intensity", "Submetering_1", "Submetering_2", "Submetering_3")
 print(correlation_mat)
 
-corrplot(correlation_mat, method = 'color', order="hclust", addCoef.col = 'black', tl.col="black", tl.srt=45, type="upper",mar=c(0.1,0.1,0.1,0.1))
+corrplot(correlation_mat, method = 'color', order="hclust", addCoef.col = 'black', tl.col="black", tl.srt=20, type="upper",mar=c(0.01,0.01,0.01,0.01))
 
 
 
