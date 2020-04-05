98ec4d5c5312ce7709b1e931bc53b1ae30c4cbbd
Jordon <jordonl@sfu.ca>
2020-04-05 15:06:01 -0700

    pca code

--- code/pca.R
+++ code/pca.R
@@ -4,40 +4,26 @@ setwd('/home/jl/cmpt318-project')
 dataSet <- read.csv("data_without_NA.txt", header = TRUE, sep = ",", dec = ".")
 #dataSet <- read.csv("test1.txt", header = TRUE, sep = ",", dec = ".")
 dataSet<- na.omit(dataSet)
-# Set the format for the weekdays
-weekdays_date = strftime(dataSet$Date, format="%u")
-dataSet$weekdays_num = as.integer(weekdays_date)
-weekdays = dataSet[dataSet$weekdays_num <= 5,]
-weekends = dataSet[dataSet$weekdays_num > 5,]
 
-weekdays_time = factor(weekdays$Time)
-weekdays_hours = hms(as.character(weekdays_time))
-weekdays$hours = as.integer(hour(weekdays_hours))
+autumnDataset <- read.csv("plot data/train_data_autumn_weekday.txt",header= TRUE)
+autumnDataset <- na.omit(autumnDataset)
 
-weekends_time = factor(weekends$Time)
-weekends_hours = hms(as.character(weekends_time))
-weekends$hours = as.integer(hour(weekends_hours))
+springDataset <- read.csv("plot data/train_data_spring_weekday.txt",header= TRUE)
+springDataset <- na.omit(springDataset)
 
-# Allot numbers to specific weekdays
-Monday = weekdays[weekdays$weekdays_num == 1,]
-Tuesday = weekdays[weekdays$weekdays_num == 2,]
-Wednesday = weekdays[weekdays$weekdays_num == 3,]
-Thursday = weekdays[weekdays$weekdays_num == 4,]
-Friday = weekdays[weekdays$weekdays_num == 5,]
-Saturday = weekends[weekends$weekdays_num == 6,]
-Sunday = weekends[weekends$weekdays_num == 7,]
+summerDataset <- read.csv("plot data/train_data_summer_weekday.txt",header= TRUE)
+summerDataset <- na.omit(summerDataset)
 
-# Categorize hours into away hours and work hours for Wednesdays and Saturdays
-# Away hours: 10 am to 1 pm
-# Home hours: 7 pm to 10 pm
-wednesday_awayhours = Wednesday[(Wednesday$hours >=10) & (Wednesday$hours <=13),]
-wednesday_homehours = Wednesday[(Wednesday$hours >=19) & (Wednesday$hours <=22),]
+winterDataset <- read.csv("plot data/train_data_wintor_weekday.txt",header= TRUE)
+winterDataset <- na.omit(winterDataset)
 
-saturday_awayhours = Saturday[(Saturday$hours >=10) & (Saturday$hours <=13),]
-saturday_homehours = Saturday[(Saturday$hours >=19) & (Saturday$hours <=22),]
+weekend <- read.csv("plot data/one_random_weekend_.txt",header= TRUE)
+weekend <- na.omit(weekend)
+weekday <- read.csv("plot data/one_random_weekday.txt",header= TRUE)
+weekday <- na.omit(weekday)
 
 get_mean<- function(data){
-  data$Date <- as.Date(data$Date, "%d/%m/%Y")
+  data$Date <- as.Date(data$Date, "%Y-%m-%d")
   mean_active_power <- aggregate(data["Global_active_power"], by=data["Date"], mean)
   mean_reactive_power <- aggregate(data["Global_reactive_power"], by=data["Date"], mean)
   mean_voltage <- aggregate(data["Voltage"], by=data["Date"], mean)
@@ -54,10 +40,19 @@ get_mean<- function(data){
   pca<- prcomp(mean_total[,2:8] , center = TRUE,scale. = TRUE)
   return(pca)
 }
-pca <-get_mean(saturday_awayhours)
+pca <-get_mean(autumnDataset)
 summary(pca)
 library(devtools)
 #install_github("vqv/ggbiplot")
 library(ggbiplot)
 
 ggbiplot(pca)
+
+pca_summer <-get_mean(summerDataset)
+ggbiplot(pca_summer)
+
+pca_spring <-get_mean(springDataset)
+ggbiplot(pca_spring)
+
+pca_winter<-get_mean(winterDataset)
+ggbiplot(pca_winter)
