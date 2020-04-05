8b95b4947e92e60884db5c5eceadebaa20e7fd7d
Jordon <jordonl@sfu.ca>
2020-04-05 01:47:12 -0700

    plot code

--- code/linearFit.R
+++ code/linearFit.R
@@ -1,43 +1,31 @@
 library(lubridate)
 library(ggplot2)
-#setwd('/home/jl/cmpt318-project')
+setwd('/home/jl/cmpt318-project')
 setwd('E:\\OneDrive - sfu.ca\\CMPT 318 Cyber Security')
 
-dataSet <- read.csv("data_without_NA.txt", header = TRUE, sep = ",", dec = ".")
+dataSet <- read.csv("data_without_NA.txt", header = FALSE, sep = ",", dec = ".")
 #dataSet <- read.csv("test1.txt", header = TRUE, sep = ",", dec = ".")
 dataSet<- na.omit(dataSet)
-# Set the format for the weekdays
-weekdays_date = strftime(dataSet$Date, format="%u")
-dataSet$weekdays_num = as.integer(weekdays_date)
-weekdays = dataSet[dataSet$weekdays_num <= 5,]
-weekends = dataSet[dataSet$weekdays_num > 5,]
+autumnDataset <- read.csv("plot data/train_data_autumn_weekday.txt",header= TRUE)
+autumnDataset <- na.omit(autumnDataset)
 
-weekdays_time = factor(weekdays$Time)
-weekdays_hours = hms(as.character(weekdays_time))
-weekdays$hours = as.integer(hour(weekdays_hours))
+springDataset <- read.csv("plot data/train_data_spring_weekday.txt",header= TRUE)
+springDataset <- na.omit(springDataset)
 
-weekends_time = factor(weekends$Time)
-weekends_hours = hms(as.character(weekends_time))
-weekends$hours = as.integer(hour(weekends_hours))
+summerDataset <- read.csv("plot data/train_data_summer_weekday.txt",header= TRUE)
+summerDataset <- na.omit(summerDataset)
 
-# Allot numbers to specific weekdays
-Monday = weekdays[weekdays$weekdays_num == 1,]
-Tuesday = weekdays[weekdays$weekdays_num == 2,]
-Wednesday = weekdays[weekdays$weekdays_num == 3,]
-Thursday = weekdays[weekdays$weekdays_num == 4,]
-Friday = weekdays[weekdays$weekdays_num == 5,]
-Saturday = weekends[weekends$weekdays_num == 6,]
-Sunday = weekends[weekends$weekdays_num == 7,]
+winterDataset <- read.csv("plot data/train_data_wintor_weekday.txt",header= TRUE)
+winterDataset <- na.omit(winterDataset)
 
-# Categorize hours into away hours and work hours for Wednesdays and Saturdays
-# Away hours: 10 am to 1 pm
-# Home hours: 7 pm to 10 pm
-wednesday_awayhours = Wednesday[(Wednesday$hours >=10) & (Wednesday$hours <=13),]
-wednesday_homehours = Wednesday[(Wednesday$hours >=19) & (Wednesday$hours <=22),]
+weekend <- read.csv("plot data/one_random_weekend_.txt",header= TRUE)
+weekend <- na.omit(weekend)
+weekday <- read.csv("plot data/one_random_weekday.txt",header= TRUE)
+weekday <- na.omit(weekday)
+awayTime <- factor(autumnDataset$Time)
 
-saturday_awayhours = Saturday[(Saturday$hours >=10) & (Saturday$hours <=13),]
-saturday_homehours = Saturday[(Saturday$hours >=19) & (Saturday$hours <=22),]
-data = saturday_awayhours
+
+keeps<-c(1,121,241,361,481,601,721,841,961,1081,1201,1321,1440) # Indices for levels you want to show
 
 get_linearPolyFit <- function(data){
   time <- factor(data$Time)
@@ -78,9 +66,11 @@ get_linearPolyFit <- function(data){
   p1 <-ggplot(data=dataMean , mapping=aes(x=Time, y=Global_active_power, group = 1)) +
     geom_point(color="black") +
     geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_linear), size=1, color="red") + 
-    geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_polynomial), size=1, color="Orange") +
-    geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_linear), size=1, color="Green") + 
-    geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_polynomial), size=1, color="Yellow")
+    geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_polynomial), size=1, color="Orange") + 
+    geom_smooth(color="Blue") +
+    scale_x_discrete(
+      breaks=levels(awayTime)[keeps],
+      labels=levels(awayTime)[keeps])
   ##
   dataMean <- data.frame("Global_reactive_power" = meanReactive, "Time" = levels(awayTime))
   
@@ -95,9 +85,11 @@ get_linearPolyFit <- function(data){
   p2 <-ggplot(data=dataMean , mapping=aes(x=Time, y=Global_reactive_power, group = 1)) +
     geom_point(color="black") +
     geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_linear), size=1, color="red") + 
-    geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_polynomial), size=1, color="Orange") +
-    geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_linear), size=1, color="Green") + 
-    geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_polynomial), size=1, color="Yellow")
+    geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_polynomial), size=1, color="Orange") + 
+    geom_smooth(color="Blue") +
+    scale_x_discrete(
+      breaks=levels(awayTime)[keeps],
+      labels=levels(awayTime)[keeps])
   ##
   dataMean <- data.frame("Voltage" = meanVoltage, "Time" = levels(awayTime))
   
@@ -112,9 +104,11 @@ get_linearPolyFit <- function(data){
   p3 <-ggplot(data=dataMean , mapping=aes(x=Time, y=Voltage, group = 1)) +
     geom_point(color="black") +
     geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_linear), size=1, color="red") + 
-    geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_polynomial), size=1, color="Orange") +
-    geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_linear), size=1, color="Green") + 
-    geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_polynomial), size=1, color="Yellow")
+    geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_polynomial), size=1, color="Orange") + 
+    geom_smooth(color="Blue") +
+    scale_x_discrete(
+      breaks=levels(awayTime)[keeps],
+      labels=levels(awayTime)[keeps])
   ##
   dataMean <- data.frame("Global_intensity" = meanIntensity, "Time" = levels(awayTime))
   
@@ -128,9 +122,11 @@ get_linearPolyFit <- function(data){
   p4 <-ggplot(data=dataMean , mapping=aes(x=Time, y=Global_intensity, group = 1)) +
     geom_point(color="black") +
     geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_linear), size=1, color="red") + 
-    geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_polynomial), size=1, color="Orange") +
-    geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_linear), size=1, color="Green") + 
-    geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_polynomial), size=1, color="Yellow") 
+    geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_polynomial), size=1, color="Orange") + 
+    geom_smooth(color="Blue") +
+    scale_x_discrete(
+      breaks=levels(awayTime)[keeps],
+      labels=levels(awayTime)[keeps])
   ##
   dataMean <- data.frame("Sub_metering_1" = meanMeter1, "Time" = levels(awayTime))
   
@@ -144,9 +140,11 @@ get_linearPolyFit <- function(data){
   p5 <-ggplot(data=dataMean , mapping=aes(x=Time, y=Sub_metering_1, group = 1)) +
     geom_point(color="black") +
     geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_linear), size=1, color="red") + 
-    geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_polynomial), size=1, color="Orange") +
-    geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_linear), size=1, color="Green") + 
-    geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_polynomial), size=1, color="Yellow") 
+    geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_polynomial), size=1, color="Orange") + 
+    geom_smooth(color="Blue") +
+    scale_x_discrete(
+      breaks=levels(awayTime)[keeps],
+      labels=levels(awayTime)[keeps])
   ##
   dataMean <- data.frame("Sub_metering_2" = meanMeter2, "Time" = levels(awayTime))
   
@@ -160,9 +158,11 @@ get_linearPolyFit <- function(data){
   p6 <-ggplot(data=dataMean , mapping=aes(x=Time, y=Sub_metering_2, group = 1)) +
     geom_point(color="black") +
     geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_linear), size=1, color="red") + 
-    geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_polynomial), size=1, color="Orange") +
-    geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_linear), size=1, color="Green") + 
-    geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_polynomial), size=1, color="Yellow") 
+    geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_polynomial), size=1, color="Orange") + 
+    geom_smooth(color="Blue") +
+    scale_x_discrete(
+      breaks=levels(awayTime)[keeps],
+      labels=levels(awayTime)[keeps])
   ##
   dataMean <- data.frame("Sub_metering_3" = meanMeter3, "Time" = levels(awayTime))
   
@@ -176,18 +176,45 @@ get_linearPolyFit <- function(data){
   p7 <-ggplot(data=dataMean , mapping=aes(x=Time, y=Sub_metering_3, group = 1)) +
     geom_point(color="black") +
     geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_linear), size=1, color="red") + 
-    geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_polynomial), size=1, color="Orange") +
-    geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_linear), size=1, color="Green") + 
-    geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_polynomial), size=1, color="Yellow") 
+    geom_line(data=dataMean , mapping=aes(x=Time, y=prediction_polynomial), size=1, color="Orange") + 
+    geom_smooth(color="Blue") +
+    scale_x_discrete(
+      breaks=levels(awayTime)[keeps],
+      labels=levels(awayTime)[keeps])
   returnList = list(p1,p2,p3,p4,p5,p6,p7)
   return(returnList)
 }
 
-plots = get_linearPolyFit(wednesday_homehours)
+plots = get_linearPolyFit(autumnDataset)
 print(plots[1])
 print(plots[2])
 print(plots[3])
 print(plots[4])
 print(plots[5])
-print(plots[6])
-print(plots[7])
+spring_plots = get_linearPolyFit(springDataset)
+print(spring_plots[1])
+print(spring_plots[2])
+print(spring_plots[4])
+
+winter_plots = get_linearPolyFit(winterDataset)
+print(winter_plots[1])
+print(winter_plots[2])
+print(winter_plots[4])
+summer_plots = get_linearPolyFit(summerDataset)
+print(summer_plots[1])
+print(summer_plots[2])
+print(summer_plots[4])
+
+awayTime <- factor(weekend$Time)
+keeps<-c(1,38,78,118,158,198,238,278,318,358,372) 
+weekend_plots = get_linearPolyFit(weekend)
+print(weekend_plots[1])
+print(weekend_plots[2])
+print(weekend_plots[4])
+
+awayTime <- factor(weekday$Time)
+keeps<-c(1,121,241,361,481,601,721,841,961,1081,1201,1321,1440) # Indices for levels you want to show
+weekday_plots = get_linearPolyFit(weekday)
+print(weekday_plots[1])
+print(weekday_plots[2])
+print(weekday_plots[4])
